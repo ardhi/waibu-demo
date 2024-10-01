cd1721b452ef21708723331cfc1f8c6c9467259a
Ardhi Lukianto <ardhi@lukianto.com>
2024-10-01 20:53:40 +0700

    Updates

--- bajo/hook/waibu-mpa.theme@after-inject-css.js
+++ bajo/hook/waibu-mpa.theme@after-inject-css.js
@@ -1,6 +1,10 @@
 async function waibuMpaThemefterInjectCss ({ items, req }) {
-  const base = req.darkMode ? 'github-dark' : 'github'
-  items.push(`waibuExtra.virtual:/highlightjs/styles/${base}.min.css`)
+  const { get } = this.app.bajo.lib._
+  const reqNs = get(req, 'routeOptions.config.ns')
+  if (reqNs === 'waibuDemo') {
+    const base = req.darkMode ? 'github-dark' : 'github'
+    items.push(`waibuExtra.virtual:/highlightjs/styles/${base}.min.css`)
+  }
 }
 
 export default waibuMpaThemefterInjectCss
--- bajo/hook/waibu-mpa.theme@after-inject-scripts.js
+++
@@ -1,9 +0,0 @@
-async function waibuMpaThemefterInjectScripts ({ items }) {
-  items.push(
-    'waibuExtra.virtual:/holderjs/holder.min.js',
-    'waibuExtra.virtual:/highlightjs/highlight.min.js',
-    'waibuExtra.virtual:/masonry/masonry.pkgd.min.js'
-  )
-}
-
-export default waibuMpaThemefterInjectScripts
